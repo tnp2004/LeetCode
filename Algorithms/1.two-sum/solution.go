3a6a323d4558f66672feaaba97536632e8960a56
tnp2004 <iceandiceisice2004@gmail.com>
2023-05-21 14:09:45 +0700

    Improve: 1. Two Sum

--- Algorithms/1.two-sum/solution.go
+++ Algorithms/1.two-sum/solution.go
@@ -1,12 +1,14 @@
 package twosum
 
 func twoSum(nums []int, target int) []int {
-	for i1, v1 := range nums {
-		for i2, v2 := range nums {
-			if i1 != i2 && v1+v2 == target {
-				return []int{i1, i2}
-			}
+	hm := map[int]int{}
+
+	for i, v := range nums {
+		_, ok := hm[v]
+		if ok {
+			return []int{i, hm[v]}
 		}
+		hm[target-v] = i
 	}
 	return nil
 }
